78e8fbc7514090f497fe6f8734edd0a1fd38ac55
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-06 19:11:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 doc = Nokogiri::HTML(open('https://flatironschool.com/'))
 
  
-courses = doc.css('inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC')
+courses = doc.css('gridContainer-EnvccM')
 
 courses.each do |course|
   puts course.text.strip
