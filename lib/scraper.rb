63f9305466302289cf2ca8ca429873daaa417a19
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-06 19:10:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open('https://flatironschool.com/')
-doc = Nokogiri::HTML(html)
+doc = Nokogiri::HTML(open('https://flatironschool.com/'))
+
  
 courses = doc.css('inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC')
 
